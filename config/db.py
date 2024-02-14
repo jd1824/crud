202a2460c9b9658d675c075e29cb031f3b84f42d
jd1824 <111320378+jd1824@users.noreply.github.com>
2024-02-14 19:53:05 -0400

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("postgresql+pymysql://root:1824@localhost:3306/test")
+engine = create_engine("sqlite:///test.db")
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
